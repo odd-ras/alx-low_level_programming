58887c506cf14ddacbba8b7699b0dc707d77d597
odd-ras <knyakango@gmail.com>
2022-12-08 10:29:48 -0800

    rev

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
-
-	puts("$char\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
