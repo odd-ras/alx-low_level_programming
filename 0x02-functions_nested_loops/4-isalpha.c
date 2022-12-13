f1c3ea3d8df046db6d083a2325d7aacd4345c28d
odd-ras <knyakango@gmail.com>
2022-12-13 08:49:22 -0800

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,13 @@
 
 /**
  * _isalpha - main entry
+ * @c: char
  * Description: checks for alphanumeric characters
  * Returns 1 if c is a letter
  * Returns 0 otherwise
  */
 int _isalpha(int c)
 {
-	char c;
-
 	if (isalpha(c) == 0)
 	{
 		_putchar('1');
