a1f1def82ff6dfeb6e02e7003bdfdc1e922df5f5
odd-ras <knyakango@gmail.com>
2022-12-15 02:07:45 -0800

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * _isupper - check for upper case letters
+ * @c: character to be checked
+ * Return: void
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
