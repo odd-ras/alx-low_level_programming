4055a068905d46774bd459cad80906eb9ed9571f
odd-ras <knyakango@gmail.com>
2023-01-05 05:51:28 -0800

    correct

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -17,19 +17,6 @@ int last_index(char *s)
 }
 
 /**
- * is_palindrome - checks for palindrome
- * @s: string
- * Return: 0 or 1
- */
-
-int is_palindrome(char *s)
-{
-	int end = last_index(s);
-
-	return (check(s, 0, end - 1, end % 2));
-}
-
-/**
  * check - palindrome checker
  * @s: string
  * @start: int
@@ -48,4 +35,17 @@ int check(char *s, int start, int end, int pair)
 		return (0);
 	else
 		return (check(s, start + 1, end - 1, pair));
+}
+
+/**
+ * is_palindrome - checks for palindrome
+ * @s: string
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+	int end = last_index(s);
+
+	return (check(s, 0, end - 1, end % 2));
 }
